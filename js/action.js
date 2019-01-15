319af583248b919c9c387fcae4945ae32f2b4d6d
Perrenoud Laura <laura.perrenoud@ecal.ch>
2019-01-15 12:14:23 +0100

    before up down navigation

--- js/action.js
+++ js/action.js
@@ -1,16 +1,5 @@
 $(document).ready(function(){
 
-
-
-  $(document).on('click','.action', function() {
-    //console.log(e);
-    console.log($(this));
-    $("#container-side").hide();
-    /*$(this).parent().addClass("hidden");
-    let idtofind = $(this).parent().attr('idC');
-    $(".dot[idC='"+idtofind+"']").removeClass("selected");*/
-  });
-
   $(document).on('click','.less', function() {
     $("#container-side").hide();
     $("#container-titre").show();
--- js/timeline-action.js
+++ js/timeline-action.js
@@ -1,55 +1,45 @@
+/*NEW ONE*/
 $(document).ready(function(){
 
-  $("#timeline-scrollable").scroll(function (event) {
-      scroll = $("#container-scrollable").scrollLeft();
-      //window.requestAnimationFrame(draw);
+  $("#container-timeline").scroll(function(event) {
+
+      let scroll = $("#container-timeline").scrollLeft();
+      let maxScroll = $("#timeline-scrollable").width();
       let normalized = map_range(scroll, 0,maxScroll, 0,1);
-      console.log("[scrolling] scroll "+scroll +"  normalized  "+normalized);
-  });
 
-  document.addEventListener("timeline-scrollable", function(e) {
-    console.log(e);
+      //seulement ppour l'instant
+      let contwidth = $("#container-timeline").width();
+      //console.log("[scrolling] scroll "+scroll +"  normalized  "+normalized);
 
-    // let normal = moment(e.detail.normal);
-    // let dotDate = moment(e.detail.date);
-    //
-    //
-    // var scroll_year = dotDate.year();
-    //
-    // for (var year = 1989; year <= scroll_year; year++) {
-    //   var selector = "." + year + ":parent";
-    //   $("." + year + ":parent").removeClass("hidden");
-    //
-    //   // diff temps
-    //   var diff = dotDate.diff(moment(year, "YYYY"));
-    //   var delta_ms = moment.duration(diff).asMilliseconds();
-    //   // console.log(d);
-    //   // console.log(d.asMilliseconds());
-    //   var max_delta = 1000.0 * 60 * 60 * 24 * 365 * 5;
-    //   // console.log(delta_ms)
-    //   //console.log(max_delta)
-    //
-    //   // console.log(delta_ms / max_delta);
-    //   var min_pct = 0.10;
-    //   var pct = Math.max(Math.min(delta_ms / max_delta, 1), 0); // clamp 0 - 1
-    //   pct = 1.0 - pct;
-    //   pct = Math.max(pct, min_pct);
-    //
-    //   var maxSize = 90;
-    //   ;var fontSize = maxSize * pct;
-    //
-    //   $("." + year + ":parent").attr("font-size", fontSize);
-    //
-    // }
-    //
-    // for (var year = scroll_year + 1; year <= 2019; year++) {
-    //   var selector = "." + year + ":parent";
-    //   $("." + year + ":parent").addClass("hidden");
-    // }
-    //
+      let rightscroll = Math.round(maxScroll-contwidth);
+      //console.log("[scrolling] scroll "+scroll + "  maxscroll "+maxScroll+" rightscroll "+rightscroll);
 
+      let startdate = moment("01/01/1989", "D/M/YYYY");
+      var enddatetest = moment("01/01/2019", "D/M/YYYY");
+      var diffSeconds = enddatetest.diff(startdate, "seconds");
+      let secondsToFind = map_range(normalized, 0, 1, 0, diffSeconds);
+      let searchdate = moment("01/01/1989", "D/M/YYYY");
+      searchdate = moment(searchdate).add(secondsToFind, "seconds");
+      //console.log("searchdate "+searchdate);
+
+      var event = new CustomEvent("timeline-scroll", {
+        detail: {
+          normal: normalized,
+          date: searchdate
+        }
+      });
+      // var event = new CustomEvent("timeline-scroll", {
+      //   detail: {
+      //     normal: normalized
+      //   }
+      // });
+      document.dispatchEvent(event);
 
   });
+
+
+
+
 
   function map_range(value, low1, high1, low2, high2) {
     return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
