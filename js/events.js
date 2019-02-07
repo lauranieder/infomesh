36fddefba2673335521644e6c4bab857d21aa299
Perrenoud Laura <laura.perrenoud@ecal.ch>
2019-02-07 17:35:14 +0100

    tooltip

--- js/events.js
+++ js/events.js
@@ -1,6 +1,6 @@
 var events = [];
-var rootStartDate = moment('1989/01/01', 'YYYY/MM/DD');
-var rootEndDate = moment('2019/12/31', 'YYYY/MM/DD');
+var rootStartDate = moment('01/01/1989', 'DD/MM/YYYY');
+var rootEndDate = moment('31/12/2019', 'DD/MM/YYYY');
 var totalTime = rootEndDate.diff(rootStartDate, 'seconds');
 var timelineWidth;
 
@@ -25,16 +25,18 @@ function computeEvents() {
   $('.event-marker').remove();
 
   $.each(events, function(index, item) {
-    var startDate = moment(item.start, 'YYYY/MM/DD');
+    var startDate = moment(item.start, 'DD/MM/YYYY');
     var endDate, className;
 
     if (item.end) {
-      endDate = moment(item.end, 'YYYY/MM/DD');
+      endDate = moment(item.end, 'DD/MM/YYYY');
       className = 'has-duration';
     } else {
       endDate = moment(startDate).add(12, 'd');
       className = 'no-duration';
     }
+
+
 
     var startTime = startDate.diff(rootStartDate, 'seconds');
     var endTime = endDate.diff(rootStartDate, 'seconds');
@@ -48,6 +50,22 @@ function computeEvents() {
     var block = $('<div id="marker-' + index + '" class="event-marker ' + className + '" style="left:calc(50vw + '+startPosition+'vw);width:'+width+'vw;"></div>');
 
     $('#timeline-scrollable').append(block);
+
+    if (item.wikifetch) {
+      wikifetching(item.wikifetch,index);
+      console.log(item.wikifetch);
+    }
+  });
+}
+
+function wikifetching(wiki, index){
+  var url = 'https://en.wikipedia.org/w/api.php?action=parse&format=json&prop=wikitext&page=Timeline_of_Facebook&section=1';
+  var url = 'https://en.wikipedia.org/api/rest_v1/page/summary/scrollbar';
+  //$.getJSON( url+'&callback=?',function(data){
+  $.getJSON( url,function(data){
+    console.log(data);
+    console.log(data.extract_html);
+    events[index].content = data.extract_html;
   });
 }
 
@@ -64,12 +82,12 @@ $(document).ready(function() {
     var currentPopupIndex = -1;
 
     $.each(events, function(index, item) {
-      var startDate = moment(item.start, 'YYYY/MM/DD')
+      var startDate = moment(item.start, 'DD/MM/YYYY')
 
       //startDate.subtract(eventPadding, 'days');
 
       if (item.end) {
-        var endDate = moment(item.end, 'YYYY/MM/DD');
+        var endDate = moment(item.end, 'DD/MM/YYYY');
       } else {
         var endDate = moment(startDate).add(12, 'd');
       }
