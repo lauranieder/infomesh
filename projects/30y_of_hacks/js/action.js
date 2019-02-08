d306814d73896f6662e82757008f583a0bce1ef3
Nathan Vogel <nathan@nathanvogel.com>
2019-02-08 16:28:12 +0100

    30y_hack :truck: Restructure project. Only 1 iframe now

--- projects/30y_of_hacks/js/action.js
+++ projects/30y_of_hacks/js/action.js
@@ -1,48 +1,21 @@
-/* globals moment */
+/* exported popupCallback timelineGoToId */
+/* globals goToBlock */
 
-var iframe;
-
+/*
+ * Called by Tardy's timeline communication scripts.
+ * Those scripts are included by my HTML.
+ * If it's defined, it:
+ * - Hides the classic timeline popup
+ * - Receive the id (index) of the current event on every
+ *   scroll event. -1 if no event is present on the timeline.
+ */
 function popupCallback(id) {
-  console.log(id, events[id]);
-  if (id >= 0) sendMessage(JSON.stringify({ id: id }));
+  // console.log(id, events[id]);
+  if (id >= 0) goToBlock(id, true);
 }
 
-// addEventListener support for IE8
-function bindEvent(element, eventName, eventHandler) {
-  if (element.addEventListener) {
-    element.addEventListener(eventName, eventHandler, false);
-  } else if (element.attachEvent) {
-    element.attachEvent("on" + eventName, eventHandler);
-  }
+// Tell the timeline to go to an event.
+function timelineGoToId(id) {
+  // Not possible yet
+  console.log("Timeline go to id:", id);
 }
-
-// Send a message to the child iframe
-function sendMessage(msg) {
-  // Make sure you are sending a string, and to stringify JSON
-  iframe.contentWindow.postMessage(msg, "*");
-}
-
-// Listen to message from child window
-bindEvent(window, "message", e => {});
-
-$(document).ready(() => {
-  // Create project iframe
-  iframe = document.createElement("iframe");
-  iframe.setAttribute("src", "./project/index.html");
-  iframe.style.width = "100%";
-  iframe.style.height = "100%";
-  // Add iframe
-  var mainContainer = document.getElementById("container-project");
-  mainContainer.style.overflowY = "hidden";
-  mainContainer.appendChild(iframe);
-
-  document.addEventListener("timeline-scroll", e => {
-    let dotDate = moment(e.detail.date);
-  });
-
-  document.addEventListener("timeline-loadDotContent", e => {
-    let dotID = e.detail.id;
-    // Communicate to my iframe
-    sendMessage(JSON.stringify({ id: dotID }));
-  });
-});
