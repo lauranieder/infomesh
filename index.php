96d3b9d7af89854b1f4f2da62b93ba8d1573c64d
Lionel Tardy <lionel.tardy@me.com>
2019-02-08 14:35:13 +0100

    Fix some local issue

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
   <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
   <script type="text/javascript" src="js/script.js"></script>
   <script>
-  var currentPagetName = '<? echo str_replace('/', '', $_SERVER['REQUEST_URI']); ?>';
+  var currentPagetName = "<? echo str_replace('/', '', $_SERVER['REQUEST_URI']); ?>";
   </script>
 </head>
 <body>
