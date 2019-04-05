3f98b0a03d3b8673fd01ff2685a4c6e57889c2bb
Perrenoud Laura <laura.perrenoud@ecal.ch>
2019-04-05 18:02:52 +0200

    styles update

--- index.php
+++ index.php
@@ -47,12 +47,12 @@
         <div id="project-credits"><p></p></div>
       </div>
       <div id="container-menu" class="content flex-bottom font-large">
-        <a href="#" id="button-open-projects">30 years of</a>
-        <a href="#" id="button-open-about" class="selected">About</a>
+        <a href="#" id="button-open-projects" class="selected">30 years of</a>
+        <a href="#" id="button-open-about" >About</a>
       </div>
     </div>
 
-    <div id="container-projects" class="container d-flex reduced">
+    <div id="container-projects" class="container d-flex main">
       <div class="project-list"></div>
     </div>
     <div id="container-main" class="container reduced">
@@ -63,7 +63,7 @@
         <?php echo file_get_contents("css/UI/UIArrowRight.svg"); ?>
       </div>
     </div>
-    <div id="container-about" class="container d-flex main background-blue">
+    <div id="container-about" class="container d-flex reduced background-blue">
       <div class="content">
         <p>The Information Mesh site celebrates the 30th anniversary of CERN's creation of the World Wide Web by creating a social, technical, cultural, and legal timeline of Web history. Within this timeline, key historical developments are brought to life through interactive experiences created by Interaction Design students at the Swiss design school ECAL. 17 students will come to San Francisco in October 2018 for a week-long study tour organized by swissnex San Francisco, where they will visit key data visualization companies and partners and begin to develop the project.</br></br>As envisioned, the timeline presents an overview of Web history, starting with the initial proposal for hypertext by Tim Berners-Lee at CERN in 1989, initially under the name "Information Mesh." From this start date, users can explore 30 years of evolution, with links to key writings and projects that create a historical guide to the social and cultural transformations the Web has unleashed.</br></br>The ECAL study group is part of the swissnex Salon, a series of activities exploring the impact of technology on fundamental societal values drawn from the preamble of the Swiss Constitution. Their projects will playfully examine the Utopian idealism at the heart of the Web, reconnecting us to the original optimism surrounding these communication technologies with a critical engagement regarding where we have arrived today. This presents an opportunity to return a collective focus to how we might bring the human back to the center of innovation.
         </p>
