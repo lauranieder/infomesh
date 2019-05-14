5f8422f7f1e74ae7c0a4391e77fde5fdf60d571e
Perrenoud Laura <laura.perrenoud@ecal.ch>
2019-05-14 16:57:47 -0700

    cleaned code events.js

--- js/events.js
+++ js/events.js
@@ -11,29 +11,82 @@ var noDurationEventSize = 30;
 var eventPadding = 30;
 var popupCallback;
 var cellwidth = 8.333;
+
+$(document).ready(function() {
+  loadEvents();
+  createPopup();
+  window.parent.postMessage({message: 'getResponsive'}, '*');
+
+  var lastPopupIndex;
+  $(window).resize(function(){
+    window.parent.postMessage({message: 'getResponsive'}, '*');
+  });
+
+  var lookForPopup = function(e) {
+    var currentPopupIndex = -1;
+    $.each(events, function(index, item) {
+      var startDate = moment(parseDate(item.start), 'DD/MM/YYYY');
+      if (item.end) {
+        var endDate = moment(item.end, 'DD/MM/YYYY');
+      } else {
+        var endDate = moment(startDate).add(noDurationEventSize, 'd');
+      }
+      startDate.subtract(eventPadding, 'days');
+      endDate = endDate.add(eventPadding, 'days');
+      if (e.detail.date.unix() >= startDate.unix() && e.detail.date.unix() <= endDate.unix()) {
+        currentPopupIndex = index;
+      }
+    });
+
+    if (lastPopupIndex != currentPopupIndex) {
+      lastPopupIndex = currentPopupIndex;
+
+      if (popupCallback) {
+        $('.event-marker').removeClass('active');
+        $('#marker-' + currentPopupIndex).addClass('active');
+        popupCallback(currentPopupIndex);
+        return;
+      }
+
+      if (currentPopupIndex == -1) {
+        $('.event-marker').removeClass('active');
+        $('#popup').removeClass('opened');
+      } else {
+        $('.event-marker').removeClass('active');
+        $('#marker-' + currentPopupIndex).addClass('active');
+        $('#popup').addClass('opened');
+        $('#popup-title').text(events[currentPopupIndex].title);
+        $('#popup-content').html(events[currentPopupIndex].content);
+        $('#popup-date').html(formatDate(events[currentPopupIndex].start));
+      }
+    }
+  };
+
+  var funcTogglePopup = function() {
+    $('#popup').toggleClass('reduced');
+    window.parent.postMessage({message: 'isPopReduced', status: $('#popup').hasClass('reduced')}, '*');
+  };
+
+  $(document).on('timeline-scroll', lookForPopup);
+  $('body').on('click', '#popup', funcTogglePopup);
+  $('body').on('touchstart', '#popup', funcTogglePopup);
+});
+
+
 function isMobileF(x){
-  //console.log("[event.js] mobile checker");
-  //console.log("isMobile = "+x);
   if (x) { // If media query matches
-    //document.body.style.backgroundColor = "yellow";
-    //console.log("mobile");
     cellwidth = 16.666;
-    //$('.timeline-containers').addClass("mobile");
     $('body').addClass("mobile");
     $('html').addClass("mobile");
   } else {
-    //document.body.style.backgroundColor = "pink";
-    //console.log("desktop");
     cellwidth = 8.333;
     $('body').removeClass("mobile");
     $('html').removeClass("mobile");
-    //$('.timeline-containers').removeClass("mobile");
   }
   displayEvents();
 }
 
 function isExtendedF(status){
-  console.log("Iframe received isExtended "+status);
   if (status) { // If side panel is hidden
     $('body').addClass("extended");
     $('html').addClass("extended");
@@ -46,87 +99,14 @@ function isExtendedF(status){
 function createPopup() {
   var popupContainer = $('<div id="popup-container"></div>');
   $('body').append(popupContainer);
-
   $('#popup-container').load('/template/popup.html');
 }
 
 function loadEvents() {
   jQuery.getJSON('./events.json', function(data) {
     events = data;
-
     displayEvents(); //calculate event pos and add them to the dom
     computeEventsContent(); //fetch wiki and automate text data
-  });
-}
-
-//combine with formatDate
-function parseDate(date){
-  var dateS = String(date);
-  var full = dateS.match(/[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{2,4}/g);
-  var month = dateS.match(/[0-9]{1,2}\/[0-9]{2,4}/g);
-  var year = dateS.match(/[0-9]{2,4}/g);
-  if(full){
-    //console.log("regular date " + date);
-    return date;
-  }else if(month){
-    //console.log("month date " + date);
-    return "01/"+date;
-  }else if(year){
-    //console.log("year date " + date);
-    return "01/01/"+date;
-  }else{
-    //console.log("CHECK DATA");
-    //alert("CHECK DATA in "+date);
-    return "01/01/1989";
-  }
-  // supposedly better regex that doesn't work https://www.sitepoint.com/jquery-basic-regex-selector-examples/  ^(0[1-9]|[12][0-9]|3[01])[- \/.](0[1-9]|1[012])[- \/.](19|20)dd$
-}
-
-function formatDate(date){
-  var dateS = String(date);
-  /*Dates can either match 1-2 digits*/
-  var full = dateS.match(/[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{2,4}/g);
-  var month = dateS.match(/[0-9]{1,2}\/[0-9]{2,4}/g);
-  var year = dateS.match(/[0-9]{2,4}/g);
-  if(full){
-    //console.log("regular date " + date);
-    return moment(date, 'DD/MM/YYYY').format('MMMM Do, YYYY');
-  }else if(month){
-    //console.log("month date " + date);
-    return moment("01/"+date , 'DD/MM/YYYY').format('MMMM YYYY');
-  }else if(year){
-    //console.log("year date " + date);
-    return moment("01/01/"+date, 'DD/MM/YYYY').format('YYYY');
-  }else{
-    //console.log("CHECK DATA");
-    //alert("CHECK DATA in "+date);
-    return moment("01/01/1989", 'DD/MM/YYYY').format('MMMM Do, YYYY');
-  }
-  // supposedly better regex that doesn't work https://www.sitepoint.com/jquery-basic-regex-selector-examples/  ^(0[1-9]|[12][0-9]|3[01])[- \/.](0[1-9]|1[012])[- \/.](19|20)dd$
-}
-
-
-//Improved this executes ONLY ONCE ! Not at every reload of the page
-function computeEventsContent(){
-  console.log("[event.js] computeEventsContent");
-  $.each(events, function(index, item) {
-    if (item.wikifetch) {
-      wikifetching(item.wikifetch, index);  /*TO PUT BACK AFTER*/
-    }
-
-    /*TO IMPROVE*/
-    console.log(item.readmore);
-    var containReadmore = item.readmore.match(/id='readmore'/g);
-    if(item.readmore != null && item.readmore != ""){
-      item.content += "</br><a target='_blank' id='readmore' href='"+item.readmore+"'>Read more</a>";
-    }
-
-    //TODO : regex to improve, replace link with target blank //doit être fait après le wikifetch ? non pas de link sauf erreur donc ok
-    if(item.content){
-      var temp = item.content;
-      var temp = temp.replace("<a", "<a target='_blank' ");
-      item.content = temp;
-    }
   });
 }
 
@@ -161,6 +141,30 @@ function displayEvents() {
   });
 }
 
+//Improved this executes ONLY ONCE ! Not at every reload of the page
+function computeEventsContent(){
+  console.log("[event.js] computeEventsContent");
+  $.each(events, function(index, item) {
+    if (item.wikifetch) {
+      wikifetching(item.wikifetch, index);  /*TO PUT BACK AFTER*/
+    }
+
+    /*TO IMPROVE*/
+    console.log(item.readmore);
+    var containReadmore = item.readmore.match(/id='readmore'/g);
+    if(item.readmore != null && item.readmore != ""){
+      item.content += "</br><a target='_blank' id='readmore' href='"+item.readmore+"'>Read more</a>";
+    }
+
+    //TODO : regex to improve, replace link with target blank //doit être fait après le wikifetch ? non pas de link sauf erreur donc ok
+    if(item.content){
+      var temp = item.content;
+      var temp = temp.replace("<a", "<a target='_blank' ");
+      item.content = temp;
+    }
+  });
+}
+
 function wikifetching(wiki, index){
   wikitoload = "https://en.wikipedia.org/api/rest_v1/page/summary/"+wiki;
   $.getJSON(wikitoload, function(data){
@@ -173,75 +177,38 @@ function wikifetching(wiki, index){
   });
 }
 
+function parseDate(date){
+  var dateS = String(date);
+  /*Dates can either match 1-2 digits*/
+  var full = dateS.match(/[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{2,4}/g);
+  var month = dateS.match(/[0-9]{1,2}\/[0-9]{2,4}/g);
+  var year = dateS.match(/[0-9]{2,4}/g);
+  if(full){
+    return date;
+  }else if(month){
+    return "01/"+date;
+  }else if(year){
+    return "01/01/"+date;
+  }else{
+    return "01/01/1989";
+  }
+  // supposedly better regex that doesn't work https://www.sitepoint.com/jquery-basic-regex-selector-examples/  ^(0[1-9]|[12][0-9]|3[01])[- \/.](0[1-9]|1[012])[- \/.](19|20)dd$
+}
 
-
-$(document).ready(function() {
-  loadEvents();
-  createPopup();
-  window.parent.postMessage({message: 'getResponsive'}, '*');
-
-  var lastPopupIndex;
-  $(window).resize(function(){
-    window.parent.postMessage({message: 'getResponsive'}, '*');
-  });
-
-
-  $(document).on('timeline-scroll', function(e) {
-    var currentPopupIndex = -1;
-
-    $.each(events, function(index, item) {
-      var startDate = moment(parseDate(item.start), 'DD/MM/YYYY');
-
-      if (item.end) {
-        var endDate = moment(item.end, 'DD/MM/YYYY');
-      } else {
-        var endDate = moment(startDate).add(noDurationEventSize, 'd');
-      }
-
-      startDate.subtract(eventPadding, 'days');
-      endDate = endDate.add(eventPadding, 'days');
-
-      if (e.detail.date.unix() >= startDate.unix() && e.detail.date.unix() <= endDate.unix()) {
-        currentPopupIndex = index;
-      }
-    });
-
-    if (lastPopupIndex != currentPopupIndex) {
-      lastPopupIndex = currentPopupIndex;
-
-      if (popupCallback) {
-        $('.event-marker').removeClass('active');
-        $('#marker-' + currentPopupIndex).addClass('active');
-
-        popupCallback(currentPopupIndex);
-        return;
-      }
-
-      if (currentPopupIndex == -1) {
-        $('.event-marker').removeClass('active');
-        $('#popup').removeClass('opened');
-      } else {
-        $('.event-marker').removeClass('active');
-        $('#marker-' + currentPopupIndex).addClass('active');
-        $('#popup').addClass('opened');
-        $('#popup-title').text(events[currentPopupIndex].title);
-        $('#popup-content').html(events[currentPopupIndex].content);
-
-        //$('#popup-date').html(moment(events[currentPopupIndex].start, 'DD/MM/YYYY').format('MMMM Do, YYYY'));
-        //console.log(events[currentPopupIndex].title);
-        $('#popup-date').html(formatDate(events[currentPopupIndex].start));
-
-
-      }
-    }
-  });
-
-
-  var funcTogglePopup = function() {
-    $('#popup').toggleClass('reduced');
-    window.parent.postMessage({message: 'isPopReduced', status: $('#popup').hasClass('reduced')}, '*');
-  };
-
-  $('body').on('click', '#popup', funcTogglePopup);
-  $('body').on('touchstart', '#popup', funcTogglePopup);
-});
+function formatDate(date){
+  var dateS = String(date);
+  /*Dates can either match 1-2 digits*/
+  var full = dateS.match(/[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{2,4}/g);
+  var month = dateS.match(/[0-9]{1,2}\/[0-9]{2,4}/g);
+  var year = dateS.match(/[0-9]{2,4}/g);
+  if(full){
+    return moment(date, 'DD/MM/YYYY').format('MMMM Do, YYYY');
+  }else if(month){
+    return moment("01/"+date , 'DD/MM/YYYY').format('MMMM YYYY');
+  }else if(year){
+    return moment("01/01/"+date, 'DD/MM/YYYY').format('YYYY');
+  }else{
+    return moment("01/01/1989", 'DD/MM/YYYY').format('MMMM Do, YYYY');
+  }
+  // supposedly better regex that doesn't work https://www.sitepoint.com/jquery-basic-regex-selector-examples/  ^(0[1-9]|[12][0-9]|3[01])[- \/.](0[1-9]|1[012])[- \/.](19|20)dd$
+}
