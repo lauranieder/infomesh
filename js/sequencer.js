bbb45d6472dba3cfbd75e23cb12e73a0cd1e5331
Tibor Udvari <tibor.udvari@icloud.com>
2019-05-15 14:40:00 +0200

    Webinfluencer with video

--- js/sequencer.js
+++ js/sequencer.js
@@ -1,89 +1,18 @@
 var Sequencer = {
-  lowresImages: [],
-  highResTimeout: 0,
-  lastFrameId: 0,
-  loadIndex: 0,
-  imageLoaded: 1,
-  loadStart: 0,
+  video: null,
 
   options: {
-    frameCount: 0,
-    folder: '',
-    baseName: '',
-    ext: 'jpg'
+    video: null
   },
 
   init: function(options) {
     this.options = $.extend(this.options, options);
-
-    this.loader();
-  },
-
-  loader: function () {
-    //console.log("loader...");
-    this.loadStart = new Date();
-
-    for (var i = 1; i < this.options.frameCount; i++) {
-      this.loadImage(i, this.options.folder + '/lowres/' +  this.options.baseName + i + '.' + this.options.ext);
-    }
-  },
-
-  loadImage: function(index, url) {
-
-    var image = new Image();
-    image.src = url;
-    //console.log("starts.. "+url);
-
-    var self = this;
-    image.onload = function() {
-      self.imageLoaded++;
-
-      //console.log(self.imageLoaded, self.options.frameCount);
-      if (self.imageLoaded == self.options.frameCount) {
-        var date = new Date();
-        //console.log('Low res loaded in ' + (date.getTime() - self.loadStart.getTime()));
-          //console.log("loaded "+url);
-      }
-    };
-
-    this.lowresImages.push(image);
+    this.video = options.video;
   },
 
   draw: function(normalizedPosition) {
-    var id = parseInt(this.map_range(normalizedPosition, 0, 1, 1, this.options.frameCount));
-
-    if (id > this.frameCount) id = this.frameCount;
-
-    if (this.lastFrameId != id) {
-      clearTimeout(this.highResTimeout);
-
-      this.drawLowres(this.options.folder + '/lowres/' +  this.options.baseName + id + '.' + this.options.ext);
-
-      var self = this;
-      this.highResTimeout = setTimeout(function() {
-        self.drawHighres(self.options.folder + '/highres/' +  self.options.baseName + id + '.' + self.options.ext);
-      }, 250);
-
-      this.lastFrameId = id;
-    }
-  },
-
-  drawLowres: function(url) {
-    $('#sequencer').css('background-image', 'url(' + url + ')');
-  },
-
-  drawHighres: function(url) {
-    this.loadIndex++;
-    var image = new Image();
-    image.src = url;
-
-    var currentIndex = this.loadIndex;
-    var self = this;
-    image.onload = function() {
-      if (self.loadIndex > currentIndex) return;
-
-      $('#sequencer').css('background-image', 'url(' + url + ')');
-    }
+    var id = parseInt(this.map_range(normalizedPosition, 0, 1, 1, 300));
+    this.video.currentTime = id;
   },
 
   map_range(value, low1, high1, low2, high2) {
